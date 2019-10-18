37f7ae445711ab806c69e6d1aab3e20b958645a2
Ivan Novikov <vanchell370@gmail.com>
2019-10-18 20:19:06 +0300

    Initial Commit

---
+++ NewsDemoJSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsDemoJSON
+//
+//  Created by Иван Новиков on 18.10.2019.
+//  Copyright © 2019 Иван Новиков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
