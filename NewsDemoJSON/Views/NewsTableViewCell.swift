18489db603c45e6c2aeb2712d336363d9d43bfd0
Ivan Novikov <vanchell370@gmail.com>
2019-10-22 13:24:14 +0300

    Добавил NewsTableViewCell и NavigationController

---
+++ NewsDemoJSON/Views/NewsTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  NewsTableViewCell.swift
+//  NewsDemoJSON
+//
+//  Created by Иван Новиков on 22.10.2019.
+//  Copyright © 2019 Иван Новиков. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+   @IBOutlet weak var newsLabel: UILabel!
+
+}
